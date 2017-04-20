29cf8b60bf51bb3c06fde5bcfa3404552446321a
Didymus5 <farnhamthomas@gmail.com>
2017-04-20 15:34:33 -0500

    Class File: readContactList() : vector<Contact>

--- IKGContacts/File.cpp
+++ IKGContacts/File.cpp
@@ -1,10 +1,18 @@
 #include "File.h"
 #include "Contact.h"
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <sstream>
+
+using namespace std;
 
 
-
-File::File()
+File::File(string fileName)
 {
+	//fstream ioFile{ fileName, ios::in | ios::out };
+
+	//if (!ioFile) {cerr << "File could not be opened" << endl; exit(EXIT_FAILURE);}
 }
 
 
@@ -17,4 +25,41 @@ Contact File::readContract(int position)
 {
 	Contact contact;
 	return contact;
+}
+
+vector<Contact> File::readContactList()
+{
+	fstream ioFile{ "sampleContacts.dat", ios::in | ios::out };
+
+	if (!ioFile) {cerr << "File could not be opened" << endl; exit(EXIT_FAILURE);}
+	
+	vector<Contact> list;
+	Contact contact;
+	string line;
+	string str;	
+	
+	while (!ioFile.eof()) {
+		
+		//getline(ioFile, line);
+		
+		while(getline(ioFile, line)) // gets a whole line from .dat file
+		{
+			std::stringstream ss(line); // line streamed
+
+			// gets string until delimiter '|'
+			getline(ss, str, '|'); contact.setTitle(str);
+			getline(ss, str, '|'); contact.setFirstName(str);
+			getline(ss, str, '|'); contact.setLastName(str);
+			getline(ss, str, '|'); contact.setNationality(str);
+			getline(ss, str, '|'); contact.setState(str);
+			getline(ss, str, '|'); contact.setCountry(str);
+			getline(ss, str, '|'); contact.setEmail(str);
+			getline(ss, str, '|'); contact.setPhoneNumber(str);
+			getline(ss, str, '|'); contact.setRace(str);
+
+			list.push_back(contact);
+		}
+	}
+
+	return list;
 }
--- IKGContacts/File.h
+++ IKGContacts/File.h
@@ -1,9 +1,19 @@
 #pragma once
+#include "Contact.h"
+#include <vector>
+#include <iostream>
+#include <string>
+#include <fstream>
+
+using namespace std;
+
 class File
 {
 public:
-	File();
+	File(string fileName = "sampleContacts.dat");
 	~File();
-	Contact
+	vector<Contact> readContactList();
+	Contact readContract(int position);
+private:
+	//fstream ioFile{"sampleContacts.dat", ios::in | ios::out };
 };
-
